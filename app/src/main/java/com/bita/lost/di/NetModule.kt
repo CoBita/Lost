9e12ae9f4e18be53313650b6cd746732ff12e126
정서윤 <seoyoon630@neobns.com>
2020-05-18 13:41:22 +0900

    빌드 오류 수정

--- app/src/main/java/com/bita/lost/di/NetModule.kt
+++ app/src/main/java/com/bita/lost/di/NetModule.kt
@@ -1,11 +1,9 @@
 package com.bita.lost.di
 
 import com.and.base.net.Net
-import com.bita.lost.net.CoronaRequestInterceptor
-import com.bita.lost.net.CoronaResponseInterceptor
-import com.bita.lost.net.HeaderInterceptor
 import com.bita.lost.net.NetConst
 import com.bita.lost.Const
+import okhttp3.Interceptor
 import org.koin.dsl.module
 
 val netModule = module {
@@ -13,7 +11,7 @@ val netModule = module {
     val baseUrl = NetConst.BASE_URl
     val serviceKey = Const.SERVICE_KEY
     val type = "json"
-    single { arrayOf(CoronaResponseInterceptor(), HeaderInterceptor(), CoronaRequestInterceptor(serviceKey, type)) }
+    single { arrayOf<Interceptor>() }
     single { Net(baseUrl, interceptors = get()) }
 
 }
--- app/src/main/java/com/bita/lost/ui/MainActivity.kt
+++ app/src/main/java/com/bita/lost/ui/MainActivity.kt
@@ -1,9 +1,13 @@
-package com.bita.lost
+package com.bita.lost.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.and.base.ui.BaseViewModel
+import com.bita.lost.R
+import com.bita.lost.base.LActivity
 
 class MainActivity : AppCompatActivity() {
+//    override lateinit var vm: BaseViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
