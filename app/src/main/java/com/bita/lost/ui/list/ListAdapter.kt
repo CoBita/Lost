d6c9e882e4acbcc769ae82df649a06a933283363
정서윤 <seoyoon630@neobns.com>
2020-05-19 15:21:39 +0900

    분실물 목록 불러오기 애니메이션 추가

--- app/src/main/java/com/bita/lost/ui/list/ListAdapter.kt
+++ app/src/main/java/com/bita/lost/ui/list/ListAdapter.kt
@@ -2,19 +2,39 @@ package com.bita.lost.ui.list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import com.bita.lost.R
 import com.bita.lost.base.BaseAdapter
 import com.bita.lost.base.BaseHolder
 import com.bita.lost.databinding.ListItemBinding
 import com.bita.lost.repo.data.LostItem
 
 class ListAdapter : BaseAdapter<LostItem>() {
+    val isAnimatedList = arrayListOf<Boolean>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseHolder<LostItem> = ListHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun getItemViewType(position: Int): Int = 0
-}
 
-class ListHolder(private val binding: ListItemBinding) : BaseHolder<LostItem>(binding.root) {
-    override fun bind(data: LostItem) {
-        binding.data = data
+    override fun set(items: ArrayList<LostItem>) {
+        super.set(items)
+        isAnimatedList.addAll(Array(items.size) { false })
+    }
+
+    override fun addAll(items: ArrayList<LostItem>) {
+        super.addAll(items)
+        isAnimatedList.addAll(Array(items.size) { false })
+    }
+
+    inner class ListHolder(private val binding: ListItemBinding) : BaseHolder<LostItem>(binding.root) {
+        override fun bind(data: LostItem) {
+            val index = items.indexOf(data)
+            if (!isAnimatedList[index]) {
+                binding.root.animation = AnimationUtils.loadAnimation(binding.root.context, R.anim.appear_anim)
+                isAnimatedList[index] = true
+            }
+            binding.data = data
+        }
     }
 }
+
--- app/src/main/java/com/bita/lost/ui/list/ListBindingAdapter.kt
+++ app/src/main/java/com/bita/lost/ui/list/ListBindingAdapter.kt
@@ -11,13 +11,13 @@ object ListBindingAdapter {
     @BindingAdapter("list:items")
     fun setAdapter(v: RecyclerView, data: ArrayList<LostItem>) {
         v.adapter?.let {
-//            (it as ListAdapter).set(data)
             (it as ListAdapter).addAll(data)
         } ?: run {
             val adapter = ListAdapter()
             adapter.set(data)
             v.adapter = adapter
         }
+        v.scheduleLayoutAnimation()
     }
 
     @JvmStatic
