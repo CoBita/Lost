23780848b3f14d39e26ec79e4c7eefff87f06c0f
정서윤 <seoyoon630@neobns.com>
2020-05-25 16:53:24 +0900

    아이템이 한개밖에 없을 때, 파싱 안되는 버그 수정

--- app/src/main/java/com/bita/lost/di/ListModule.kt
+++ app/src/main/java/com/bita/lost/di/ListModule.kt
@@ -6,11 +6,14 @@ import com.bita.lost.repo.ListRepository
 import com.bita.lost.repo.ListRepositoryImpl
 import com.bita.lost.ui.list.ListViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
+const val listNet = "listNet"
 
 val listModule
     get() = module {
-        single { createNetService<ListDataSource>(get()) }
+        single { createNetService<ListDataSource>(get(qualifier = named(listNet))) }
         single<ListRepository> { ListRepositoryImpl(get()) }
 
         viewModel { ListViewModel(get()) }
--- app/src/main/java/com/bita/lost/di/NetModule.kt
+++ app/src/main/java/com/bita/lost/di/NetModule.kt
@@ -7,11 +7,13 @@ import com.bita.lost.net.LostRequestInterceptor
 import com.bita.lost.net.LostResponseInterceptor
 import com.bita.lost.net.NetConst
 import okhttp3.Interceptor
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val netModule = module {
     val baseUrl = NetConst.BASE_URl
     single { arrayOf(HeaderInterceptor(), LostRequestInterceptor(Const.SERVICE_KEY, "json"), LostResponseInterceptor()) }
-    single { Net(baseUrl, interceptors = get()) }
+    single(qualifier = named(listNet)) { Net(baseUrl, interceptors = get()) }
+    single { Net(baseUrl) }
 
 }
