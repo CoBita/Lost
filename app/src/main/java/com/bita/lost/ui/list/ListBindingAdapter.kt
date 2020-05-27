9a47fadd78f7108d8fb96a31e6bb513c36435660
정서윤 <seoyoon630@neobns.com>
2020-05-27 14:05:23 +0900

    직접입력했을 때 입력한 키워드가 보이도록 수정

--- app/src/main/java/com/bita/lost/ui/list/ListBindingAdapter.kt
+++ app/src/main/java/com/bita/lost/ui/list/ListBindingAdapter.kt
@@ -1,15 +1,25 @@
 package com.bita.lost.ui.list
 
+import android.graphics.Color
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bita.lost.common.textAsBitmap
+import com.bita.lost.repo.data.AcquirePlaceCode
+import com.bita.lost.repo.data.AcquisitionCode
 import com.bita.lost.repo.data.LostItem
 
 object ListBindingAdapter {
 
     @JvmStatic
     @BindingAdapter("app:items", "app:has_next", "app:get_next_data", "app:is_load_finish")
-    fun setAdapter(v: RecyclerView, rawData: ArrayList<LostItem>, hasNext: Boolean, function: () -> Unit, isLoadFinish: Boolean) {
+    fun setAdapter(
+        v: RecyclerView,
+        rawData: ArrayList<LostItem>,
+        hasNext: Boolean,
+        function: () -> Unit,
+        isLoadFinish: Boolean
+    ) {
         if (!isLoadFinish) {
             return
         }
@@ -21,11 +31,14 @@ object ListBindingAdapter {
         }
 
         v.adapter?.let { it as? ListAdapter }?.set(data)
-                ?: run {
-                    val adapter = ListAdapter({ id -> v.context.let { it as? ListActivity }?.showDetail(id) }, function)
-                    adapter.set(data)
-                    v.adapter = adapter
-                }
+            ?: run {
+                val adapter = ListAdapter(
+                    { id -> v.context.let { it as? ListActivity }?.showDetail(id) },
+                    function
+                )
+                adapter.set(data)
+                v.adapter = adapter
+            }
         v.scheduleLayoutAnimation()
     }
 
@@ -33,5 +46,21 @@ object ListBindingAdapter {
     @BindingAdapter("app:src")
     fun setSrc(v: ImageView, drawableId: Int) {
         v.setImageResource(drawableId)
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:setSrcByAcquirePlaceCode")
+    fun setSrcByAcquirePlaceCode(v: ImageView, enum: AcquirePlaceCode) {
+        if (enum == AcquirePlaceCode.직접입력) {
+            v.setImageBitmap(enum.description.textAsBitmap(v.context, 10f, Color.BLACK))
+        } else setSrc(v, enum.icon)
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:setSrcByAcquisitionCode")
+    fun setSrcByAcquisitionCode(v: ImageView, enum: AcquisitionCode) {
+        if (enum == AcquisitionCode.직접입력) {
+            v.setImageBitmap(enum.description.textAsBitmap(v.context, 10f, Color.BLACK))
+        } else setSrc(v, enum.icon)
     }
 }
