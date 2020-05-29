13fcecc8c544d75e46d490191d0a5be593364e7f
정서윤 <seoyoon630@neobns.com>
2020-05-29 17:14:16 +0900

    컬러코드 추가

---
+++ app/src/main/java/com/bita/lost/repo/data/ColorCode.kt
@@ -0,0 +1,19 @@
+@file:Suppress("NonAsciiCharacters")
+
+package com.bita.lost.repo.data
+
+import android.graphics.Color
+
+enum class ColorCode(val hex: Int, val code: String, val description: String) {
+    흰색(Color.parseColor("fff"), "CL1001", "화이트(하양)"),
+    검정색(Color.parseColor("000"), "CL1002", "블랙(검정)"),
+    빨강색(Color.parseColor("f00"), "CL1003", "레드(빨강)"),
+    주황색(Color.parseColor("ff8000"), "CL1004", "오렌지(주황)"),
+    노랑색(Color.parseColor("ff0"), "CL1005", "옐로우(노랑)"),
+    초록색(Color.parseColor("0f0"), "CL1006", "그린(초록)"),
+    파랑색(Color.parseColor("00f"), "CL1007", "블루(파랑)"),
+    갈색(Color.parseColor("a52a2a"), "CL1008", "브라운(갈)"),
+    보라색(Color.parseColor("ee82ee"), "CL1009", "바이올렛(보라)"),
+    분홍색(Color.parseColor("ffc0cb"), "CL1011", "핑크(분홍)"),
+    기타(Color.parseColor("fff"), "CL1010", "기타");
+}
