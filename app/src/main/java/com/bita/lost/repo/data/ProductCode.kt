49ace857c373f366c88985f44c4b6666838e12be
정서윤 <seoyoon630@neobns.com>
2020-06-01 10:00:41 +0900

    상품코드테이블 추가

---
+++ app/src/main/java/com/bita/lost/repo/data/ProductCode.kt
@@ -0,0 +1,25 @@
+@file:Suppress("NonAsciiCharacters")
+
+package com.bita.lost.repo.data
+
+enum class ProductCode(val code: String) {
+    가방("PRA000"),
+    귀금속("PRO000"),
+    기타물품("PRZ000"),
+    도서용품("PRB000"),
+    산업용품("PRD000"),
+    서류("PRC000"),
+    쇼핑백("PRQ000"),
+    스포츠용품("PRE000"),
+    악기("PRR000"),
+    유가증권("PRM000"),
+    의류("PRK000"),
+    자동차("PRF000"),
+    전자기기("PRG000"),
+    증명서("PRN000"),
+    지갑("PRH000"),
+    카드("PRP000"),
+    컴퓨터("PRI000"),
+    현금("PRL000"),
+    휴대폰("PRJ000")
+}
