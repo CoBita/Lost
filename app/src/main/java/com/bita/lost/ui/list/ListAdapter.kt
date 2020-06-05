6b57ce9f3d1fed45b086da7969077cb13543eda5
정서윤 <seoyoon630@neobns.com>
2020-06-05 15:31:26 +0900

    습득물 목록 헤더 UI 수정

--- app/src/main/java/com/bita/lost/ui/list/ListAdapter.kt
+++ app/src/main/java/com/bita/lost/ui/list/ListAdapter.kt
@@ -8,7 +8,6 @@ import com.bita.lost.R
 import com.bita.lost.base.BaseAdapter
 import com.bita.lost.base.BaseHolder
 import com.bita.lost.databinding.ListItemBinding
-import com.bita.lost.databinding.ListMoreItemBinding
 import com.bita.lost.databinding.ListNoItemBinding
 import com.bita.lost.repo.data.LostItem
 
@@ -18,20 +17,17 @@ class ListAdapter(private val showDetail: (id: String, seq: Int) -> Unit) : Base
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseHolder<Any> {
         return when (viewType) {
             0 -> ListHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-            1 -> NoHolder(ListNoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-            else -> MoreHolder(ListMoreItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            else -> NoHolder(ListNoItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
         }
     }
 
-    override fun getItemViewType(position: Int): Int =
-            when (getItem(position)) {
-                is LostItem -> 0
-                BaseHolderType.결과없음 -> 1
-                BaseHolderType.더보기 -> 2
-                else -> 2
-            }
+    override fun getItemViewType(position: Int): Int = when (getItem(position)) {
+        is LostItem -> 0
+        BaseHolderType.결과없음 -> 1
+        else -> 1
+    }
 
-
+    // item 애니메이션
     private fun animate(v: View, index: Int) {
         if (lastAnimatedIndex < index) {
             v.animation = AnimationUtils.loadAnimation(v.context, R.anim.up_anim)
@@ -52,18 +48,6 @@ class ListAdapter(private val showDetail: (id: String, seq: Int) -> Unit) : Base
     inner class NoHolder(private val binding: ListNoItemBinding) : BaseHolder<Any>(binding.root) {
         override fun bind(data: Any) {
             animate(binding.notice, items.indexOf(data))
-        }
-    }
-
-    inner class MoreHolder(private val binding: ListMoreItemBinding) : BaseHolder<Any>(binding.root) {
-        override fun bind(data: Any) {
-            animate(binding.root, items.indexOf(data))
-            binding.root.setOnClickListener {
-                lastAnimatedIndex--
-                val lastIndex = items.lastIndex
-                items.removeAt(lastIndex)
-                notifyItemRemoved(lastIndex)
-            }
         }
     }
 }
