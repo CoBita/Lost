27d868f84ca5b7ba23bed816e98bf700b352645c
박상혁 <psh161@naver.com>
2020-06-06 15:27:04 +0900

    - Detail Fr 변수 연결

--- app/src/main/java/com/bita/lost/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/bita/lost/ui/detail/DetailFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import com.bita.lost.R
 import com.bita.lost.base.LFragment
 import com.bita.lost.databinding.DetailFrBinding
@@ -32,6 +31,7 @@ class DetailFragment : LFragment() {
 
     override fun onLoadOnce() {
         super.onLoadOnce()
+        binding.vm = vm
     }
 
     companion object {
